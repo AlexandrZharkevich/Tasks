2b325e451108ee0529e43036db99a27da7d6101a
Aleksandr Zharkevich <alexandr.zharkevich@gmail.com>
2020-02-01 13:35:24 +0300

    InsertionSort added

--- Sort.py
+++ Sort.py
@@ -13,32 +13,15 @@ def BubbleSort(A):  # сортировка пузырьком
                 A[j + 1] = a
 
 
-def QuickSort(A, fst, lst):  # быстрая сортировка
-    if fst >= lst:
-        return
-
-    # i, j = fst, lst
-    pivot = A[fst]
-    # pivot = A[random.randint(fst, lst)]
-    first_bigger = fst + 1
-    while first_bigger <= lst:
-        if A[first_bigger] >= pivot:
-            break
-        first_bigger += 1
-    i = first_bigger + 1
-    while i <= lst:
-        if A[i] < pivot:
-            A[i], A[first_bigger] = A[first_bigger], A[i]
-            first_bigger += 1
-        i += 1
-
-    last_smaller = first_bigger - 1
-    A[fst], A[last_smaller] = A[last_smaller], A[fst]
-    QuickSort(A, fst, last_smaller - 1)
-    QuickSort(A, first_bigger, lst)
+def InsertionSort(A):  # сортировка вставками
+    for i in range(len(A) - 1):
+        j = i + 1
+        while j > 0 and A[j] > A[j - 1]:
+            A[j], A[j - 1] = A[j - 1], A[j]
+            j -= 1
 
 
-table = prettytable.PrettyTable(["Размер списка", "Время пузырька", "Время быстрой"])
+table = prettytable.PrettyTable(["Размер списка", "Время пузырька", "Время вставки"])
 x = []
 y1 = []
 y2 = []
@@ -51,18 +34,7 @@ for N in range(1000, 5001, 1000):
     for i in range(N):
         A.append(int(round(random.random() * (max - min) + min)))
 
-    # print(A)
-
     B = A.copy()
-    # print(B)
-
-    # BubbleSort(A)
-    print("---")
-    # print(A)
-
-    # QuickSort(B, 0, len(B)-1)
-    print("---")
-    # print(B)
 
     t1 = datetime.datetime.now()
     BubbleSort(A)
@@ -71,10 +43,10 @@ for N in range(1000, 5001, 1000):
     print("Пузырьковая сортировка   " + str(N) + "   заняла   " + str((t2 - t1).total_seconds()) + "c")
 
     t3 = datetime.datetime.now()
-    QuickSort(B, 0, len(B) - 1)
+    InsertionSort(B)
     t4 = datetime.datetime.now()
     y2.append((t4 - t3).total_seconds())
-    print("Быстрая   " + str(N) + "   заняла   " + str((t4 - t3).total_seconds()) + "c")
+    print("Вставками   " + str(N) + "   заняла   " + str((t4 - t3).total_seconds()) + "c")
 
     table.add_row([str(N), str((t2 - t1).total_seconds()), str((t4 - t3).total_seconds())])
 print(table)
